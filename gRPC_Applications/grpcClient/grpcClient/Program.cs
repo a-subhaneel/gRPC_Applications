4bacdde7e422e17031b4e093b36afa96d535fd52
Subhaneel Agarwalla <59724085+a-subhaneel@users.noreply.github.com>
2023-03-30 19:49:59 +0530

    Add files via upload

---
+++ gRPC_Applications/grpcClient/grpcClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace grpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Press any key to continue....");
+            string readData = Console.ReadLine();
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = readData });
+            Console.WriteLine($"Greetings: {reply.Message}");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
